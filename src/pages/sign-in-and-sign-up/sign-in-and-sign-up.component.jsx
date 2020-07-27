b50e44f1a86c72f0dd912078600cc5657e79acc9
Mona Lisa <david.t.bui83@gmail.com>
2020-07-26 20:57:12 -0700

    added signin and signup page and signin component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SingIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAdnSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SingIn />
+  </div>
+);
+
+export default SignInAdnSignUpPage;
